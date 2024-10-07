ef8dc5538511c49349d31bdf9ea36c0fb9186536
kiran reddy <51226611+kiranreddym76@users.noreply.github.com>
2024-10-07 11:58:23 +0530

    Initial commit

---
+++ src/main/java/com/montree/oneapp/OneappApplication.java
@@ -0,0 +1,13 @@
+package com.montree.oneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montree/oneapp/OneappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montree.oneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
